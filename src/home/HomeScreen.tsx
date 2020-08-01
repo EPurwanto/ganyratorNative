cae6e5e2ffe3140b059a491be47e8d1abc6e900c
Edward Purwanto <edward.purwanto@students.mq.edu.au>
2020-08-01 23:34:28 +1000

    HomeScreen Layout

--- src/home/HomeScreen.tsx
+++ src/home/HomeScreen.tsx
@@ -1,22 +1,46 @@
 import {NavigationContainer} from "@react-navigation/native";
-import {Button, StyleProp, Text, View, ViewStyle} from "react-native";
+import {Button, Picker, StyleProp, Text, View, ViewStyle} from "react-native";
 import {StatusBar} from "expo-status-bar";
 import AppContext from "../utils/AppContext";
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import App from "../../App";
+import styles from "../utils/AppStyles";
 
 
 export default function () {
     const context = useContext(AppContext);
+    const [selected, setSelected] = useState<string>("");
 
     return (
             <View style={context.styles.container}>
                 <StatusBar style="auto" />
-                <Text>Session: {context.text}</Text>
-                <Text>Loaded: {context.loaded ? "true": "false"}</Text>
-                <Button title={"Hello"} onPress={() => {context.setText("Hello")}}/>
-                <Button title={"Goodbye"} onPress={() => {context.setText("Goodbye")}}/>
-                <Button title={"G'Day"} onPress={() => {context.setText("G'Day")}}/>
+                <View style={context.styles.rollArea}>
+                    <Text style={context.styles.text}>Session: {context.text}</Text>
+                </View>
+                <View style={context.styles.btnArea}>
+                    <View style={context.styles.rollPickerBtn}>
+                        <View style={context.styles.rollPicker}>
+                            <Picker selectedValue={selected}
+                                    onValueChange={(value, index) => setSelected(value)}>
+                                {
+                                    Array.from({ length: 30 }, (_, i) => i).map((i) => <Picker.Item key={i} label={i.toString()} value={i}/>)
+                                }
+                            </Picker>
+                        </View>
+                        <View style={context.styles.rollBtn}>
+                            <Button title={"Set"}
+                                    onPress={() => {context.setText(selected)}}/>
+                        </View>
+                    </View>
+                    <View style={context.styles.btn}>
+                        <Button title={"Goodbye"}
+                                onPress={() => {context.setText("Goodbye")}}/>
+                    </View>
+                    <View style={context.styles.btn}>
+                        <Button title={"G'Day"}
+                                onPress={() => {context.setText("G'Day")}}/>
+                    </View>
+                </View>
             </View>
         )
 }
--- src/utils/AppStyles.ts
+++ src/utils/AppStyles.ts
@@ -1,16 +1,45 @@
-import {StyleSheet, ViewStyle} from "react-native";
+import {StyleSheet, TextStyle, ViewStyle} from "react-native";
 
 export interface IAppStyles {
-    container: ViewStyle;
+    container: ViewStyle,
+    rollArea: ViewStyle,
+    rollPickerBtn: ViewStyle,
+    rollPicker: ViewStyle,
+    rollBtn: ViewStyle,
+    btnArea: ViewStyle,
+    btn: ViewStyle,
+    text: TextStyle,
 }
 
 const styles = StyleSheet.create<IAppStyles>({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        paddingHorizontal: 15,
     },
+    rollArea: {
+        flexGrow: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    rollPickerBtn: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        // backgroundColor: "grey",
+    },
+    rollPicker: {
+        flexGrow: 1
+    },
+    rollBtn: {
+    },
+    btnArea: {
+    },
+    btn: {
+        marginVertical: 5
+    },
+    text: {
+        flex: 0,
+    }
 });
 
 export default styles;
